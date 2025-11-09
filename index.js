fbe7c6ef6cc419008950b324d44908ea2bc94789
Victor Prokhorov <mail@victorprokhorov.com>
2025-11-09 13:10:05 +0100

    annotate subtype relationship

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
+// https://www.youtube.com/watch?v=zmvznP1lv3E
+// https://www.youtube.com/watch?v=FdFBYUQCuHQ
 Object.defineProperty(exports, "__esModule", { value: true });
 class Shape {
     x;
@@ -20,6 +22,13 @@ class ColoredCircle extends Circle {
         this.color = color;
     }
 }
+// subtype/supertype describe relationsip between types
+// Circle is subtype of Shape i.e. Circle is a more specific Shape
+// ColoredCircle is subtype of Circle
+// Shape is supertype of Circle i.e. Shape is a more generic Circle
+// Circle is supertype of ColoredCircle
+// https://en.wikipedia.org/wiki/Liskov_substitution_principle
+// object may be replaced by sub-object without breaking the program
 const coloredCircle = new ColoredCircle("black", 10, 0);
 console.log(coloredCircle);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+// https://www.youtube.com/watch?v=zmvznP1lv3E
+// https://www.youtube.com/watch?v=FdFBYUQCuHQ
+
 class Shape {
 	x: number;
 	constructor(x: number) {
@@ -20,6 +23,14 @@ class ColoredCircle extends Circle {
 		this.color = color;
 	}
 }
+
+// subtype/supertype describe the relationship between types
+// Circle is a subtype of Shape i.e. Circle is a more specific Shape
+// ColoredCircle is a subtype of Circle
+// Shape is a supertype of Circle i.e. Shape is a more generic Circle
+// Circle is a supertype of ColoredCircle
+// https://en.wikipedia.org/wiki/Liskov_substitution_principle
+// an object may be replaced by subobject without breaking the program
 
 const coloredCircle = new ColoredCircle("black", 10, 0);
 console.log(coloredCircle);
